242c9a001adf0247b364f5667ccd0e9a7beb91b5
Akash53xyz <140371852+Akash53xyz@users.noreply.github.com>
2026-02-09 00:46:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,5 +30,22 @@ function generate() {
 }
 
 function downloadPDF() {
-  html2pdf().from(document.getElementById("resume")).save("resume.pdf");
+  const resume = document.getElementById("resume");
+
+  const options = {
+    margin: 0,
+    filename: "resume.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: {
+      scale: 2,
+      useCORS: true
+    },
+    jsPDF: {
+      unit: "mm",
+      format: "a4",
+      orientation: "portrait"
+    }
+  };
+
+  html2pdf().set(options).from(resume).save();
 }
